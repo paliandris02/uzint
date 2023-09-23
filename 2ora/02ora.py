c424c0bcf9944dda4beac1dcc5a74fea7a6ba2e3
Páli András <pali.andris@gmail.com>
2023-09-23 22:42:20 +0200

    Add files via upload

--- 2ora/02ora.py
+++ 2ora/02ora.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 import sqlite3
-import roman
-from datetime import datetime
-import matplotlib.pyplot as plt
+#import roman
+#from datetime import datetime
+#import matplotlib.pyplot as plt
 
 
 conn = sqlite3.connect('magazine_subscriptions.db')
@@ -26,6 +26,8 @@ df_subs['PRODUCT'].value_counts()
 # Folytonos számértékű: WEEKLY_FEE Előfizetés heti díja dollárban
 
 # 2.1 b)
+df_subs['TECHNICAL_COMMENCEMENT_DATE'].min() # 2016 02 01
+df_subs['TECHNICAL_COMMENCEMENT_DATE'].max() # 2020 03 01
 df_subs_groupby_TECHNICAL_COMMENCEMENT_DATE = df_subs.groupby("TECHNICAL_COMMENCEMENT_DATE").agg(
         Elemszam = ("TECHNICAL_COMMENCEMENT_DATE", "count"),
 )
@@ -34,19 +36,23 @@ df_subs_groupby_PRODUCT = df_subs.groupby("PRODUCT").agg(
         Elemszam = ("PRODUCT", "count"),
 )
 
+df_subs['WEEKLY_FEE'].max() # 5.3
+df_subs['WEEKLY_FEE'].min() # 0.0022
 df_subs['WEEKLY_FEE'].hist()
 
 df_clients = pd.read_sql_query('select * from Clients', conn)
 df_clients.info()
-
 df_clients.describe(include="all")
 df_clients.groupby(['BIRTH_DATE'])['ClientID'].count()
 
+
 df_clients['BIRTH_DATE_YEAR'] = [i[0:4] if i[0:4].isnumeric() ==True else '19'+i[-2:] for i in df_clients['BIRTH_DATE']]
-#df_clients['BIRTH_DATE_MONTH'] = df_clients['BIRTH_DATE'].apply(lambda x: convert_roman_month_to_normal(x))
+df_clients['BIRTH_DATE_YEAR'].max() # 2002 18
+df_clients['BIRTH_DATE_YEAR'].min() # 1906 114
 
-df_clients['BIRTH_DATE_YEAR'].hist(bins=8)
+df_clients['BIRTH_DATE_YEAR'].hist(bins=8) # ebből sok minden nem látszik
 
+# gyakorisági tábla
 gyaktábla = np.histogram(df_clients['BIRTH_DATE_YEAR'].astype(int), bins=10)
 gyaktábla = pd.DataFrame(gyaktábla).transpose()
 gyaktábla.columns =(['Gyakoriság', 'Alsó határ'])
@@ -54,6 +60,7 @@ gyaktábla['Felső határ'] = gyaktábla['Alsó határ'].shift(-1)
 gyaktábla = gyaktábla[['Alsó határ','Felső határ', 'Gyakoriság']]
 gyaktábla = gyaktábla.drop(10, axis = 'index')
 
+#df_clients['BIRTH_DATE_MONTH'] = df_clients['BIRTH_DATE'].apply(lambda x: convert_roman_month_to_normal(x))
 #def convert_roman_month_to_normal(input_date):
 #   if(":" in input_date):
 #       return int(input_date[5:7])
